01b31d4306979963579764617c81f106d861a46d
Matas <matas.lankelis@gmail.com>
2025-03-13 21:37:31 +0200

    Created initial Workout entity

---
+++ fitnessweb.Domain/Entities/Workout.cs
@@ -0,0 +1,12 @@
+﻿using fitnessweb.Domain.Types;
+
+namespace fitnessweb.Domain.Entities;
+
+public class Workout :Entity
+{
+    public required FitnessLevel Difficulty { get; set; }
+    public required ICollection<Muscle> Muscles { get; set; }
+    public required int DurationMinutes { get; set; }
+    public required ICollection<Equipment> Equipment { get; set; }
+    public required Goal Goal { get; set; }
+}

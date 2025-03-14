3c20f89c65639d5c2c9ead782aa03c2d96107607
Matas <matas.lankelis@gmail.com>
2025-03-14 16:51:56 +0200

    Updated the workout entity to include list of workoutexercises, with their own sets and reps amount

--- fitnessweb.Domain/Entities/Workout.cs
+++ fitnessweb.Domain/Entities/Workout.cs
@@ -2,11 +2,14 @@
 
 namespace fitnessweb.Domain.Entities;
 
-public class Workout :Entity
+public class Workout : Entity
 {
+    public required string Name { get; set; }
     public required FitnessLevel Difficulty { get; set; }
     public required ICollection<Muscle> Muscles { get; set; }
-    public required int DurationMinutes { get; set; }
+    public required int TargetDurationMinutes { get; set; }
     public required ICollection<Equipment> Equipment { get; set; }
     public required Goal Goal { get; set; }
+    
+    public required ICollection<WorkoutExercise> WorkoutExercises { get; set; } = new List<WorkoutExercise>();
 }

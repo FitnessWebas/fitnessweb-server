2dffbf9ae159b45c1e0fd8314e6b9b71b77de770
Matas <matas.lankelis@gmail.com>
2025-03-15 15:37:01 +0200

    Added new list to MuscleGroup

--- fitnessweb.Domain/Entities/MuscleGroup.cs
+++ fitnessweb.Domain/Entities/MuscleGroup.cs
@@ -3,5 +3,5 @@
 public class MuscleGroup : Entity
 {
     public required string MuscleGroupName { get; set; }
-    public ICollection<Muscle> Muscles { get; set; }
+    public ICollection<Muscle> Muscles { get; set; } = new List<Muscle>();
 }

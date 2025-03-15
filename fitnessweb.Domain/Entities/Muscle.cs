dd63c6199de18dbb408f64da9943facffdf778e6
Matas <matas.lankelis@gmail.com>
2025-03-16 01:46:11 +0200

    Added list of exercises to muscle entity and updated migration

--- fitnessweb.Domain/Entities/Muscle.cs
+++ fitnessweb.Domain/Entities/Muscle.cs
@@ -5,4 +5,6 @@ public class Muscle : Entity
     public MuscleGroup MuscleGroup { get; set; }
     public required string Name { get; set; }
     public Guid MuscleGroupId { get; set; }
+    
+    public ICollection<Exercise>? Exercises { get; set; }
 }

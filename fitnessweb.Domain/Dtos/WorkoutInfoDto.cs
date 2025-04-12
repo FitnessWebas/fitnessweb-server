06572a544f9828d890f365d191ea397362b50c15
Matas <matas.lankelis@gmail.com>
2025-04-12 18:39:27 +0300

    Added workout Dto

--- fitnessweb.Domain/Dtos/WorkoutInfoDto.cs
+++ fitnessweb.Domain/Dtos/WorkoutInfoDto.cs
@@ -7,11 +7,12 @@ public class WorkoutInfoDto
 {
     public Guid Id { get; set; }
     public Guid UserId { get; set; }
+    public DateTime DateOfCreation { get; set; }
     public string Name { get; set; }
     public FitnessLevel Difficulty { get; set; }
-    public int TargetDurationMinutes { get; set; }
     public ICollection<Equipment> Equipment { get; set; }
+    public int TargetDurationMinutes { get; set; }
     public Goal Goal { get; set; }
-    public ICollection<MuscleDto> Muscles { get; set; }
+    public ICollection<MuscleGroupDto> MuscleGroups { get; set; }
     public ICollection<WorkoutExerciseDto> WorkoutExercises { get; set; }
 }

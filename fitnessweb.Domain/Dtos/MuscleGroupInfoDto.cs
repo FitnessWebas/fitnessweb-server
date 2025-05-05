7f47062e23d667763b6484b8716a9f25c4897b50
jonvabalis <tmotiejus@gmail.com>
2025-05-06 00:01:43 +0300

    fix: json properties in MuscleDtos match system

--- fitnessweb.Domain/Dtos/MuscleGroupInfoDto.cs
+++ fitnessweb.Domain/Dtos/MuscleGroupInfoDto.cs
@@ -1,8 +1,14 @@
-﻿namespace fitnessweb.Domain.Dtos;
+﻿using System.Text.Json.Serialization;
+
+namespace fitnessweb.Domain.Dtos;
 
 public class MuscleGroupInfoDto
 {
+    [JsonPropertyName("id")]
     public Guid Id { get; set; }
+    
+    [JsonPropertyName("name")]
     public string Name { get; set; }
+    [JsonPropertyName("muscles")]
     public ICollection<MuscleDto> Muscles { get; set; }
 }
